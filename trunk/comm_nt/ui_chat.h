020d81fadf4279b3432502cb8606ee9ac26af8db
konrad.balys <konrad.balys@08f01046-b83b-11de-9b33-83dc4fd2bb11>
2009-11-08 01:01:02 +0000

    Small GUI change.

--- trunk/comm_nt/ui_chat.h
+++ trunk/comm_nt/ui_chat.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'chat.ui'
 **
-** Created: Sun 8. Nov 00:45:27 2009
+** Created: Sun 8. Nov 01:30:32 2009
 **      by: Qt User Interface Compiler version 4.5.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
