4d0135fc7851f27149b9c5628e22a69be5938d55
konrad.balys <konrad.balys@08f01046-b83b-11de-9b33-83dc4fd2bb11>
2010-01-11 21:37:09 +0000

    Threads and semaphores rewritten.

--- trunk/Unix/comm_nt_server/BasicSystemClasses.h
+++ trunk/Unix/comm_nt_server/BasicSystemClasses.h
@@ -1,114 +1,122 @@
 #ifndef BASIC_SYSTEM_CLASSES_H
 #define BASIC_SYSTEM_CLASSES_H
 #include "stdafx.h"
+#include "pthread.h"
+#include "semaphore.h"
 
-//! Represents all objects which have handles
+#include "pthread.h"
+#include "semaphore.h"
+#include "xutility"
+
+
+//! for compatibility
 class SysHandle{
-	HANDLE Handle;
 public:
-	SysHandle(){
-		Handle = INVALID_HANDLE_VALUE;
-	}
-	SysHandle(HANDLE h):Handle(h){}
+	SysHandle(){}
 	virtual ~SysHandle(){}
-	HANDLE GetHandle() {
-		return Handle;
-	}
-	void SetHandle(HANDLE h) {
-		Handle = h;
-	}
 };
-//! Windows Thread class
-class SysThread: public SysHandle{
-	DWORD ThreadID;	
-public: 
-	bool Exit;
-	unsigned long (__stdcall *Function) (void *);
-	SysThread(unsigned long (__stdcall *f) (void *)): SysHandle(INVALID_HANDLE_VALUE){
-		Function = f;
-		Exit = false;
-		SetHandle(CreateThread(NULL, 0, Function, 0, CREATE_SUSPENDED, &ThreadID)); 
-		if (GetHandle() == INVALID_HANDLE_VALUE || GetHandle() == 0){
-			//b³¹d
-		}
-	}
-	SysThread(unsigned long (__stdcall *f) (void *), void * params): SysHandle(INVALID_HANDLE_VALUE){
-		Function = f;
-		Exit = false;
-		SetHandle(CreateThread(NULL, 0, Function, params, CREATE_SUSPENDED, &ThreadID)); 
-		if (GetHandle() == INVALID_HANDLE_VALUE || GetHandle() == 0){
-			//b³¹d
-		}
-	}
-	void Start(){
-		if (GetHandle() != 0 && GetHandle() != INVALID_HANDLE_VALUE){
-			ResumeThread(GetHandle());
-		}
-	}
-	void Finish(){
-		if (GetHandle() != 0 && GetHandle() != INVALID_HANDLE_VALUE){
 
-			Exit = true;
-		}
-	}
-	void Suspend(){
-		if (GetHandle() != 0 && GetHandle() != INVALID_HANDLE_VALUE){
-			SuspendThread(GetHandle());
-		}
-	}
-	
-	void Join(){
-		if (GetHandle() != 0 && GetHandle() != INVALID_HANDLE_VALUE){
-			WaitForSingleObject(GetHandle(),INFINITE);
-		}
-	}
-	void Terminate(){
-		if (GetHandle() != 0 && GetHandle() != INVALID_HANDLE_VALUE){
-			TerminateThread(GetHandle(),1);
-		}
-	}
-	~SysThread(){
-		if (GetHandle() != 0 && GetHandle() != INVALID_HANDLE_VALUE){
-			CloseHandle(GetHandle());
-			SetHandle(0);
-		}
-	}
-};
-//! Windows Semaphore class
+//! universal semaphore class
 class SysSemaphore: public SysHandle{
-        int StartingValue ;
-        int MaxValue;
+	sem_t SemID;
+    int StartingValue ;
+    int MaxValue;
 public:
-        SysSemaphore(int start, int max):SysHandle(INVALID_HANDLE_VALUE){
+        SysSemaphore(int start, int max){
+			sem_init(&SemID, 0, start);
             StartingValue = start;
             MaxValue = max;
-            ;
-		SetHandle(CreateSemaphore(NULL, start, max, NULL));
-		if (GetHandle() == INVALID_HANDLE_VALUE || GetHandle() == 0){
-			//b³¹d
-			throw "blad konstruktora" ;
-		}
 	}
 	void Wait(){
-		if (GetHandle() != 0 && GetHandle() != INVALID_HANDLE_VALUE){
-			unsigned long result = WaitForSingleObject(GetHandle(),INFINITE);
-			if ( result == WAIT_OBJECT_0){
-				//MessageBox(0,L"asdf",L"asdf",0);
-			}
-		}
+		sem_wait(&SemID);
 	}
 	void Release(){
-		if (GetHandle() != 0 && GetHandle() != INVALID_HANDLE_VALUE){
-			ReleaseSemaphore(GetHandle(),1,NULL);
-		}
+		sem_post(&SemID);
 	}
 	~SysSemaphore(){
-		if (GetHandle() != 0 && GetHandle() != INVALID_HANDLE_VALUE){
-			CloseHandle(GetHandle());
-			SetHandle(0);
+		sem_destroy(&SemID);
+	}
+} * temp;
+
+//! universal thread class
+class SysThread: public SysHandle{
+	pthread_t ThreadID;	
+	std::pair<SysThread*, void*> * toDelete;
+public: 
+	int ready_to_go;
+	pthread_mutex_t suspend_mutex;
+	pthread_cond_t suspend_cond;
+	bool Exit;
+	void* (* Function) (void *arg);
+	static void *BasicFunction(void *arg) {		
+		std::pair<SysThread*, void*>* params = static_cast<std::pair<SysThread*, void*>*>(arg);		
+		SysThread * me = params->first;
+		pthread_mutex_lock(&me->suspend_mutex);
+		while (me->ready_to_go == 0) {
+			pthread_cond_wait(&me->suspend_cond, &me->suspend_mutex);
+		}
+		pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, 0);
+		pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, 0);
+		pthread_mutex_unlock(&me->suspend_mutex);
+		me->Function(params->second);
+		return NULL;
+	}
+  
+	SysThread(void* (* f) (void *)): toDelete(0), 
+		ready_to_go(0), Exit(false),
+		suspend_mutex(PTHREAD_MUTEX_INITIALIZER), 
+		suspend_cond(PTHREAD_COND_INITIALIZER){
+			Function = f;
+			pthread_attr_t attr;
+			pthread_attr_init(&attr);
+			pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+			std::pair<SysThread*, void*>* params = new std::pair<SysThread*, void*>();
+			params->first = this;
+			params->second = 0;
+			toDelete = params;
+			pthread_create(&ThreadID, &attr, BasicFunction, params);
+			pthread_attr_destroy(&attr);
+	}
+	SysThread(void* (* f) (void *), void *arg):toDelete(0),
+		ready_to_go(0), Exit(false),
+		suspend_mutex(PTHREAD_MUTEX_INITIALIZER), 
+		suspend_cond(PTHREAD_COND_INITIALIZER){
+			Function = f;
+			pthread_attr_t attr;	
+			pthread_attr_init(&attr);
+			pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+			std::pair<SysThread*, void*>* params = new std::pair<SysThread*, void*>();
+			params->first = this;
+			params->second = arg;
+			toDelete = params;
+			pthread_create(&ThreadID, &attr, BasicFunction, params);
+			pthread_attr_destroy(&attr);
+	}
+	void Start(){
+			pthread_mutex_lock(&suspend_mutex);
+			ready_to_go = 1;
+			pthread_mutex_unlock(&suspend_mutex);
+			pthread_cond_signal(&suspend_cond);
+	}
+	void Finish(){
+			Exit = true;
+	}
+	void Join(){
+		void * status;
+		pthread_join(ThreadID, &status);
+	}
+	void Terminate(){
+		pthread_cancel(ThreadID);
+	}
+	~SysThread(){
+		if (toDelete != 0) {
+			if (toDelete->second != 0)
+				delete toDelete->second;
+			delete toDelete;
+			toDelete = 0;
 		}
 	}
 };
-
-//		SearchThread = new SysThread(FilesSearcherThreadFnc,this);
 #endif
